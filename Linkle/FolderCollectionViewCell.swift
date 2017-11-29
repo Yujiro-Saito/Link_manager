2885c500c98745c4ca153ce92f357a18ae23ab77
Yujiro Saito <yujiro@Yujiro-Saito-no-MacBook-Air.local>
2017-11-29 22:54:39 +0900

    Add horizontal collectionview

---
+++ Linkle/FolderCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FolderCollectionViewCell.swift
+//  Linkle
+//
+//  Created by  Yujiro Saito on 2017/11/29.
+//  Copyright © 2017年 yujiro_saito. All rights reserved.
+//
+
+import UIKit
+
+class FolderCollectionViewCell: UICollectionViewCell {
+    
+    //Property
+    @IBOutlet weak var folder_name: UILabel!
+    
+}
--- Linkle/ViewController.swift
+++ Linkle/ViewController.swift
@@ -8,10 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    //Property
+    @IBOutlet weak var folder_collection: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Delegate
+        self.folder_collection.delegate = self
+        self.folder_collection.dataSource = self
     }
+    
+    
+    
+    
+    //Collection View methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        //Cell
+        let cell = self.folder_collection.dequeueReusableCell(withReuseIdentifier: "folder_collection", for: indexPath) as? FolderCollectionViewCell
+        cell?.folder_name.text = "Data_Science"
+        
+        return cell!
+        
+    }
+    
+    
+    
+    
 }
 
